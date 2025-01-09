610496c82dff998c9c4390628e30e4f15045a439
Bartłomiej Grzesik <bgrzesik@google.com>
2025-01-09 13:16:45 +0100

    lib: Add hypervisor module
    This change adds a new module to lib called hypervisor. This is a tiny
    module that currently just exposes a MMIO guard mapping functionality from
    hypervisor_backends crates as a two functions for both C and Rust
    respectively. The errors are naively translated to little kernel errors.
    
    hypervisor_backends currently only supports aarch64 target only. For
    this reason currently mapping works for aarch64. Once hypervisor_backends
    supports x86_64, the support in this module will have to be updated.
    
    Bug: 371136851
    Test: vsock works in pVM
    Change-Id: I5a59ac3d9048eeb66ff95fa49002e0f8a3e97727
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

---
+++ lib/libhypervisor/src/lib.rs
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Google Inc. All rights reserved
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#![no_std]
+#![feature(cfg_version)]
+// unsigned_is_multiple_of feature was added in Rust 1.82
+#![cfg_attr(version("1.82"), feature(unsigned_is_multiple_of))]
+
+use core::ffi::c_int;
+
+use rust_support::paddr_t;
+use rust_support::Error as LkError;
+
+#[cfg(target_arch = "aarch64")]
+mod hyp;
+
+// TODO: Enable for x86_64 once it's supported.
+#[cfg(target_arch = "aarch64")]
+pub use hyp::mmio_map_region;
+
+/// # Safety
+/// Actually not unsafe for targets other then aarch64
+#[cfg(not(target_arch = "aarch64"))]
+pub unsafe fn mmio_map_region(_paddr: usize, _size: usize) -> Result<(), LkError> {
+    Err(LkError::ERR_NOT_SUPPORTED)
+}
+
+/// # Safety
+///  - paddr must be a valid physical address
+///  - paddr + size must be a valid physical address
+///  - the caller must be aware that after the call the [paddr .. paddr + size] memory
+///    is available for reading by the host.
+#[no_mangle]
+pub unsafe extern "C" fn hypervisor_mmio_map_region(paddr: paddr_t, size: usize) -> c_int {
+    crate::mmio_map_region(paddr, size).err().unwrap_or(LkError::NO_ERROR).into()
+}
